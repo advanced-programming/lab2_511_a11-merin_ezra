ab0eaa02f2332c8672109f3e730ca15f7e1e7a44
Ezra-Fikru Asfaw <65423750+ezraasfaw@users.noreply.github.com>
2021-10-05 12:36:45 -0400

    Update soft_com.c
    lab2 1.4v

--- lab_multi_task/soft_com.c
+++ lab_multi_task/soft_com.c
@@ -1,12 +1,15 @@
 #include "console32.h"
 #include "Tick_core.h"
 #include "soft_com.h"
+
 //#include "queue.h"
 
-#define BAUD_RATE 200000
+#define BAUD_RATE 500
 //#define TICK_SECOND 80000000
-#define TIMEOUT_START   BAUD_RATE/2
-#define TIMEOUT_STOP    BAUD_RATE/4
+#define TIMEOUT_START TICKS_PER_SECOND/BAUD_RATE
+#define TIMEOUT TICKS_PER_SECOND/BAUD_RATE
+#define TIMEOUT_STOP  TICKS_PER_SECOND/(BAUD_RATE*4)
+
 
 
 
@@ -14,8 +17,8 @@
 
 //static QueueHandle_t* queue;
 static int32_t lastTick;
-int count = 0;
-
+static int count = 0;
+static int test = 0;
 /* Initializes resources */ 
 
 void initSoftCom(void){ 
@@ -34,8 +37,6 @@ void initSoftCom(void){
 /* Public interface to communicate with this task */ 
 
 int getCnt(void){ 
-    if (count==9){
-    count =1;}
 
 return count; 
 
@@ -48,22 +49,20 @@ return count;
 void softComTask(void) { 
 //int32_t rx=0;
 //int32_t delay = 10; //temporary vale for counter
+    //int size = sizeof(cChar);
 static char cChar[]="three";
-//int size = sizeof(cChar);
 static int i;
-
 static int size = 5;
-static int new_value = 'a';
-static int acsii_value;
+static int acsii_value= 'a';
 //int mask_acsii_value;
 
-//int serial[9];
+
 
 //queue = xQueueCreate(1);
 
 
 
- 	static enum {SM_POLL, SM_POLL_HANDLER, SM_RETR_ENTRY, SM_START_BIT_ENTRY,SM_START_BIT_HANDLER, SM_SEND_BITS_ENTRY,SM_SEND_BITS_HANDLER, SM_STOP_BIT_ENTRY, SM_STOP_BIT_HANDLER} state= SM_SEND_BITS_ENTRY;
+ 	static enum {SM_POLL, SM_POLL_HANDLER, SM_RETR_ENTRY, SM_START_BIT_ENTRY,SM_START_BIT_HANDLER, SM_SEND_BITS_ENTRY,SM_SEND_BITS_HANDLER, SM_STOP_BIT_ENTRY, SM_STOP_BIT_HANDLER} state= SM_START_BIT_ENTRY;
 
  
 
@@ -82,24 +81,40 @@ static int acsii_value;
         case SM_RETR_ENTRY:
             //state = SM_POLL_HANDLER;
             
-                if (i<=size){ //temporary counter
-                    cChar[i] = new_value;
-                    i++;
-                    state = SM_RETR_ENTRY;  
-                }
-                else state = SM_POLL_HANDLER;
+//                if (i<=size){ //temporary counter
+//                    cChar[i] = acsii_value;
+//                    i++;
+//                    state = SM_RETR_ENTRY;  
+//                }
+//                else state = SM_POLL_HANDLER;
             
-           
+           state = SM_START_BIT_ENTRY;
             break;
             
-        
+             
+            
+            
+            
+            
     //////////////SM_START_BIT/////////////////          
         case SM_START_BIT_ENTRY:
             LATFbits.LATF5 = 0; 
-            state = SM_START_BIT_HANDLER;
+            state = SM_SEND_BITS_HANDLER;
+            lastTick=TickGet();
             break;
               
-            
+//    /////////////////TEST///////////////////     
+//            
+       case SM_START_BIT_HANDLER:
+            if(TickDiff(lastTick)>= TIMEOUT_START){
+                lastTick=TickGet();                
+                state = SM_START_BIT_HANDLER;
+            }
+            else 
+                state = SM_SEND_BITS_ENTRY;
+            break;
+//
+    //////////////////////////////////////////////////           
             
             
 
@@ -109,16 +124,17 @@ static int acsii_value;
     //////////////SM_SEND_BITS/////////////////  
             
         case SM_SEND_BITS_ENTRY:
-            acsii_value = new_value;
-            count = 1;
-            LATFbits.LATF5 = acsii_value & 0x01; //mask
+            
+            LATFbits.LATF5 = acsii_value & 0x01; //mask            
+            lastTick=TickGet();
+            count = 0;
             state = SM_SEND_BITS_HANDLER;   
             break;
-
+ 
             
             
         case SM_SEND_BITS_HANDLER:
-         if(TickDiff(lastTick)>= TIMEOUT_START){    
+         if(TickDiff(lastTick)>= TIMEOUT){    
             lastTick=TickGet();
             
             if( count < 8){
@@ -128,16 +144,13 @@ static int acsii_value;
                 count++;
                 state = SM_SEND_BITS_HANDLER;
             }
-            
-            state = SM_STOP_BIT_ENTRY;
-         }
-         else {
+            else{
                 lastTick = TickGet();
                 state = SM_STOP_BIT_ENTRY;
-                SM_SEND_BITS_HANDLER;
-                break;  
-                
             }
+ 
+            }
+            break; 
      
         
             
@@ -146,6 +159,7 @@ static int acsii_value;
         case SM_STOP_BIT_ENTRY:
             LATFbits.LATF5 = 1; 
             state = SM_STOP_BIT_HANDLER;
+            lastTick=TickGet();
             break;
             
         case SM_STOP_BIT_HANDLER:
